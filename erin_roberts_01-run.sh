f31fa02290f0014cbf6373c5f5104aadf1762d69
Erin Roberts <erin_roberts@my.uri.edu>
2017-02-14 14:09:37 -0500

    nothing else to add

--- erin_roberts_01-run.sh
+++ erin_roberts_01-run.sh
@@ -1,4 +1,4 @@
-#hello
+#hello, happy Valentine's day
 # Usage: this script was from Assignment 1
 #Line below this is called a shebang, it indicates to command line which 
 #program to use to execute the script. Thus, the line below tells command line
